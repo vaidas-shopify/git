fd7dd6b4c97df9d78c7f16520a4afc73f95e869f
Vaidas Pilkauskas <vaidas.pilkauskas@shopify.com>
2025-12-15 16:16:36 +0200

    Revert "remote-curl: fix memory leak in show_http_message()"
    This reverts commit 7639fa3ae895d7121d190a08a8223a9ecdc47703.

--- remote-curl.c
+++ remote-curl.c
@@ -371,7 +371,6 @@ static int show_http_message(struct strbuf *type, struct strbuf *charset,
 			     struct strbuf *msg)
 {
 	const char *p, *eol;
-	struct strbuf msgbuf = STRBUF_INIT;
 
 	/*
 	 * We only show text/plain parts, as other types are likely
@@ -379,24 +378,19 @@ static int show_http_message(struct strbuf *type, struct strbuf *charset,
 	 */
 	if (strcmp(type->buf, "text/plain"))
 		return -1;
-
-	strbuf_addbuf(&msgbuf, msg);
 	if (charset->len)
-		strbuf_reencode(&msgbuf, charset->buf, get_log_output_encoding());
+		strbuf_reencode(msg, charset->buf, get_log_output_encoding());
 
-	strbuf_trim(&msgbuf);
-	if (!msgbuf.len) {
-		strbuf_release(&msgbuf);
+	strbuf_trim(msg);
+	if (!msg->len)
 		return -1;
-	}
 
-	p = msgbuf.buf;
+	p = msg->buf;
 	do {
 		eol = strchrnul(p, '\n');
 		fprintf(stderr, "remote: %.*s\n", (int)(eol - p), p);
 		p = eol + 1;
 	} while(*eol);
-	strbuf_release(&msgbuf);
 	return 0;
 }
 
